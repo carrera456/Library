3eaf91bdce0687cf83941f96841bdffb2f124b62
carrera456 <91748268+carrera456@users.noreply.github.com>
2025-10-14 23:06:33 +0200

    exercici finalitzar-lo

--- javascript.js
+++ javascript.js
@@ -6,6 +6,17 @@ function Book(title,author,pages,state,synopsis) {
   this.pages = pages;
   this.state = state;
   this.synopsis = synopsis;
+  this.id = crypto.randomUUID();
+  this.changeStatus = function() {
+    switch(this.state){
+    case 'Pendent' :
+      this.state = 'Llegit';
+      break;
+    case 'Llegit':
+      this.state = 'Pendent';
+      break;
+    }
+  }
    
 }
 
@@ -16,19 +27,41 @@ function addBookToLibrary(title,author,pages,state,synopsis) {
     
 }
 
+function removeBookFromLibrary(id){
+  myLibrary.splice(myLibrary.findIndex(obj => obj.id === id),1);
+}
+
 //creem uns quants llibres per mostrar en pantalla
 addBookToLibrary("El Hobbit","J.R.R Tolkien","356","Pendent","Les aventures de Frodo Bolson");
 addBookToLibrary("El senyor dels Anells","J.R.R Tolkien","2542","Pendent","La comunitat de l'anell");
 addBookToLibrary("Tríptic", "Eva Baltasar", "342", "Llegit", "Històries de maternitat");
 addBookToLibrary("El petit príncep", "Antoine de Sant-Excupéry", "85", "Pendent", "La història del petit príncep");
 
-function displayBooks(array){
 
-  for (const element of array){
+function removeElementsByClass(name){
+ for( var i = document.querySelector(name) ;  i != null ; i = document.querySelector(name))
+ {
+	 i.remove();
+ }
+}
+
+function removeElementById(id){
+  var doc = document.getElementById(id);
+  doc.remove();
+}
+
+function displayBooks(array){
+// crear divs per ensenyar els llibres en pantalla
+  //esborrem totes les divisions amb class card
+  removeElementsByClass(".card");
+  //girem l'array perquè els afegim en ordre invers
+  arrayReversed = array.toReversed();
+  for (const element of arrayReversed){
     const container = document.querySelector(".container");
     //creem nova div
     const divCard = document.createElement("div");
     divCard.classList.add("card");
+    divCard.id = element.id;
     //titol
     const divTitolLabel = document.createElement("div");
     divTitolLabel.classList.add("label");
@@ -71,10 +104,15 @@ function displayBooks(array){
     divEstatLabel.textContent = "Estat";
     divCard.appendChild(divEstatLabel);
     const divEstatInfo = document.createElement("div");
-    divEstatInfo.classList.add("info");
+    divEstatInfo.classList.add("estatCard");
     divEstatInfo.textContent = element.state;
     divCard.appendChild(divEstatInfo);
-
+    const buttonChangeState = document.createElement("button");
+    buttonChangeState.classList.add("changeState");
+    divCard.appendChild(buttonChangeState);
+    const buttonRemove = document.createElement("button");
+    buttonRemove.classList.add("remove");
+    divCard.appendChild(buttonRemove);
 
     //afegim a contenidor
     // container.appendChild(divCard);
@@ -87,7 +125,6 @@ function displayBooks(array){
 displayBooks(myLibrary);
 
 //obrim dialog
-
 const dialog = document.querySelector("dialog");
 const addButton = document.querySelector(".addButton");
 const closeButton = document.querySelector(".close");
@@ -100,3 +137,35 @@ closeButton.addEventListener("click",() =>{
 });
 
 
+//lògica per botó de afegir llibre
+const submit = document.querySelector(".addBook");
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  var form = new FormData(document.querySelector("form"));
+  var formElement = document.querySelector("form");
+  var valid = formElement.reportValidity();
+  if (valid == true)
+	{
+		addBookToLibrary(form.get("titol"), form.get("autor"), form.get("pagines"), form.get("estat"), form.get("sinopsis"));
+		dialog.close();
+		displayBooks(myLibrary);
+		document.querySelector("form").reset();
+	}
+})
+
+//lògica per botó de esborrar
+const container = document.querySelector(".container");
+container.addEventListener("click", (e) => {
+  let target = e.target;
+  switch(target.className){
+  case 'remove' :
+    removeElementById(target.parentElement.id);
+    removeBookFromLibrary(target.parentElement.id);
+    break;
+  case 'changeState':
+    myLibrary[myLibrary.findIndex(obj => obj.id == target.parentElement.id)].changeStatus();
+    displayBooks(myLibrary);
+  }
+  
+  
+})
